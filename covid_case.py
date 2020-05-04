6a32304add34e277577c9a409cca8f7c67debab4
bDrwx <a.bodosov@gmail.com>
2020-05-04 18:31:37 +0000

    Change data from source

--- covid_case.py
+++ covid_case.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, PrimaryKeyConstraint
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, PrimaryKeyConstraint
 Base = declarative_base()
 
 
@@ -7,32 +7,39 @@ class CovidCase(Base):
 
     __tablename__ = 'covidcase'
 
-    state = Column(String)
+    title = Column(String)
+    code = Column(String)
+    is_city = Column(Boolean)
+    coord_x = Column(Integer)
+    coord_y = Column(Integer)
     sick = Column(Integer)
     healed = Column(Integer)
-    die = Column(Integer)
+    died = Column(Integer)
+    sick_incr = Column(Integer)
+    healed_incr = Column(Integer)
+    died_incr = Column(Integer)
     date = Column(DateTime)
     __table_args__ = (
-        PrimaryKeyConstraint('date', 'state'),
+        PrimaryKeyConstraint('date', 'code'),
         {},
     )
 
-    def __init__(self, state, date):
-        self.state = state
+    def __init__(self, title, date):
+        self.title = title
         self.date = date
 
     def __setitem__(self, key, value):
         setattr(self, key, value)    
 
     def __repr__(self):
-        return f'{self.state}:{self.sick}|{self.healed}|{self.die} - {self.date}'
+        return f'{self.title}:{self.sick}|{self.healed}|{self.die} - {self.date}'
 
     def get_stats(self, obj):
-        if self.state != obj.state:
+        if self.title != obj.title:
             return None
         return (self.sick - obj.sick, self.healed - obj.healed, self.die - obj.die) 
 
     @classmethod
-    def find_by_name(cls, session, state):
-        return session.query(cls).filter(cls.state.like("%{}%".format(state))).order_by(cls.date.desc()).limit(2).all()
+    def find_by_name(cls, session, title):
+        return session.query(cls).filter(cls.title.like("%{}%".format(title))).order_by(cls.date.desc()).limit(2).all()
 
--- main.py
+++ main.py
@@ -8,10 +8,11 @@ from sqlalchemy.orm import sessionmaker
 from covid_case import Base, CovidCase
 from sqlalchemy import create_engine
 import telebot
+import json
 
 
 def init_db():
-    engine = create_engine('sqlite:///test.db', echo=True)
+    engine = create_engine('sqlite:///test_upd.db', echo=True)
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
@@ -28,33 +29,34 @@ def save_to_base(session, state_list):
 
 
 def get_data_from_web():
-
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    headers = {'User-Agent': user_agent}
     # Sorce data from parsing
-    pagedata = requests.get("https://xn--80aesfpebagmfblc0a.xn--p1ai/")
+    pagedata = requests.get("https://xn--80aesfpebagmfblc0a.xn--p1ai/information/",headers=headers)
     soup = BeautifulSoup(pagedata.text, features="html.parser")
+    with open('sorce.html', 'w') as file:
+        file.write(soup.prettify())
 
-    banner_div = soup.findAll("div", {"class": "cv-banner__description"})
+    banner_div = soup.find("small")
+    # TODO Log this in debug mode
+    print(banner_div.text)
 
     # Parse datetime from string По состоянию на 03 апреля 10:30
-    human_date = re.match(r"^По состоянию на ([\d]{2}\s[\w]{3,6}\s[\d]{2}:[\d]{2})$", banner_div[0].text).group(1)
-    updated_time = dateparser.parse(human_date)
+    human_date = re.match(r"^По состоянию на ([\d]{2}\s[\w]{3,6}\s[\d]{2}:[\d]{2})$", banner_div.text).group(1)
+    updated_time = {'date': dateparser.parse(human_date)}
 
-    state_list = []
-    map_div = soup.find_all("div", {"class": "d-map__list"})
-    # print(f"{type(soup)} and {type(map_div)}")
-    tr_list = map_div[0].find_all("tr")
-    for tr in tr_list:
-        covid_case = CovidCase(state=tr.th.text, date=updated_time)
-        columns = tr.find_all('td')
-        for column in columns:
-            result = re.search(r"^d-map__indicator_([\w]+)$", column.span['class'][1])
-            covid_case[result.group(1)] = column.text
-        state_list.append(covid_case)
-    return state_list
+    map_div = soup.find("cv-spread-overview")
+    state_list = json.loads(map_div[':spread-data'])
+    new_state_list = [{**state, **updated_time} for state in state_list]
+    
+    return new_state_list
+
 
 def main():
     session = init_db()
     covid_case_list = get_data_from_web()
+    for state in covid_case_list:
+        print(state)
     save_to_base(session, covid_case_list)
     bot = telebot.TeleBot(os.environ.get('BOT_KEY'))
 
@@ -71,7 +73,15 @@ def main():
     @bot.message_handler(content_types=['text'])
     def get_covid_stats_by_region(message):
         cc = CovidCase.find_by_name(session, message.text)
-        bot.send_message(message.from_user.id, cc[0])
+        sick, healed, die = cc[0].get_stats(cc[1])
+
+        text = (
+            f'{cc[0].title} по состоянию на {cc[0].date}:\n'
+            f'Выявлено:     {cc[0].sick:4}  +{sick:3} за последние сутки\n'
+            f'Выздоровели:  {cc[0].healed:4}    +{healed:3} за последние сутки\n'
+            f'Умерли:       {cc[0].die:4}   +{die:3} за последние сутки'
+        )
+        bot.send_message(message.from_user.id, text.encode('utf-8'))
         session.close()
 
     bot.polling(none_stop=True, interval=0)
